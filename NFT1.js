12ab2e810dff0a7a16642713194923d0473f19a4
Pankaj kumar <ps9417888445@gmail.com>
2024-06-21 22:46:20 +0530

    updated Code

--- NFT1.js
+++ NFT1.js
@@ -1,47 +1,34 @@
-/*
-Assessment Requirements
-1. Create a variable that can hold a number of NFT's. What type of variable might this be?
-2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
-   The metadata values will be passed to the function as parameters. When the NFT is ready, 
-   you will store it in the variable you created in step 1
-3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
-4. For good measure, getTotalSupply() should return the number of NFT's you have created
-*/
 
-// Step 1: Create a variable to hold your NFT's
+
 let nftCollection = [];
-let totalNFTsMinted = 0;
+let count = 0;
 
-// Step 2: Create a function that will mint an NFT
-function mintNFT(tokenName, tokenDescription, tokenImage) {
-  const nft = {
-    name: tokenName,
-    description: tokenDescription,
-    image: tokenImage
+function mintNFT(AccountHolder, AccoutNO, Bankname) {
+  const data = {
+    AccountHolder: AccountHolder,
+    AccoutNO: AccoutNO,
+    Bankname: Bankname
   };
-  nftCollection.push(nft);
-  totalNFTsMinted++;
+  nftCollection.push(data);
+  count++;
 }
-
-// Step 3: Create a function that will list all NFTs
 function listNFTs() {
   for (let i = 0; i < nftCollection.length; i++) {
-    const nft = nftCollection[i];
-    console.log("Name: " + nft.name);
-    console.log("Description: " + nft.description);
-    console.log("Image: " + nft.image);
-    console.log("------------------");
+    const data = nftCollection[i];
+    console.log("Accounter_Name: " + data.AccountHolder);
+    console.log("Account_Number: " + data.AccoutNO);
+    console.log("Bank Name: " + data.Bankname);
+    console.log("\n------------------\n");
   }
 }
 
-// Step 4: Create a function that will return the total supply of NFTs
 function getTotalSupply() {
-  console.log("Total Supply: " + totalNFTsMinted);
+  console.log("Total Supply: " + count);
 }
 
-// Call your functions below this line
-mintNFT("NFT 1", "First NFT", "image1.jpg");
-mintNFT("NFT 2", "Second NFT", "image2.jpg");
+mintNFT("Pankaj", "1019170635", "State Bank of India");
+mintNFT("Harsh", "1212164512", "Punjab National bank");
+mintNFT("Prabhat", "325696845", "Indian Bank ");
 
 listNFTs();
 getTotalSupply();
